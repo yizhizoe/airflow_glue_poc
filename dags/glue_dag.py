d2728ee72c99daa68955048403bda9af3f548fdf
Zoe Gao <yizhigao@amazon.com>
2021-06-01 22:42:16 +0800

    added iam policy example

--- dags/glue_dag.py
+++ dags/glue_dag.py
@@ -1,42 +1,14 @@
-
 from datetime import timedelta
-
-# The DAG object; we'll need this to instantiate a DAG
 from airflow import DAG
 from airflow.providers.amazon.aws.hooks.base_aws import AwsBaseHook
-# Operators; we need this to operate!
-from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
 import time
-# These args will get passed on to each operator
-# You can override them on a per-task basis during operator initialization
+import json
+import boto3, botocore
 
-azk_flow = {
-  "project" : "azkaban-test-project",
-  "nodes" : [ {
-    "id" : "test-final",
-    "type" : "command",
-    "in" : [ "test-job-4" ]
-  },
-      {
-    "id" : "test-job-start",
-    "type" : "java"
-  }, {
-    "id" : "test-job-3",
-    "type" : "java",
-    "in" : [ "test-job-2" ]
-  }, {
-    "id" : "test-job-4",
-    "type" : "java",
-    "in" : [ "test-job-3" ]
-  }, {
-    "id" : "test-job-2",
-    "type" : "java",
-    "in" : [ "test-job-start" ]
-  } ],
-  "flow" : "test",
-  "projectId" : 193
-}
+INPUT_WORKFLOW_FILE = 'sample_workflow.json'
+BUCKET_NAME = 'glue-mwaa-zoe'
 
 default_args = {
     'owner': 'airflow',
@@ -47,30 +19,11 @@ default_args = {
     'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
-    # 'queue': 'bash_queue',
-    # 'pool': 'backfill',
-    # 'priority_weight': 10,
-    # 'end_date': datetime(2016, 1, 1),
-    # 'wait_for_downstream': False,
-    # 'dag': dag,
-    # 'sla': timedelta(hours=2),
-    # 'execution_timeout': timedelta(seconds=300),
-    # 'on_failure_callback': some_function,
-    # 'on_success_callback': some_other_function,
-    # 'on_retry_callback': another_function,
-    # 'sla_miss_callback': yet_another_function,
-    # 'trigger_rule': 'all_success'
 }
-dag = DAG(
-    'glue_job_dag',
-    default_args=default_args,
-    description='PoC of glue job DAG translated from Azkaban workflow json',
-    schedule_interval=timedelta(days=1),
-)
 
-def get_glue_operator_args(job_name, script_s3_path = None, worker_number = 5):
+def get_glue_operator_args(job_name, worker_number = 5):
     glue_client = AwsBaseHook(aws_conn_id='aws_default', client_type='glue').get_client_type(client_type='glue',
-                                                                                             region_name='us-east-2')
+                                                                                             region_name='us-west-2')
     response = glue_client.start_job_run(JobName=job_name)
     job_id = response['JobRunId']
     print("Job {} ID: {}".format(job_name,job_id))
@@ -83,23 +36,44 @@ def get_glue_operator_args(job_name, script_s3_path = None, worker_number = 5):
         if state in ['STOPPED', 'FAILED', 'TIMEOUT', 'STOPPING']:
             print('Glue job {} run ID {} is in {} state'.format(job_name,job_id, state))
             raise Exception
-        time.sleep(20)
+        time.sleep(10)
 
-dag.doc_md = __doc__
-glue_tasks = dict()
-# operator over the existing Glue jobs
-for node in azk_flow['nodes']:
-    job_name = node['id']
-    glue_tasks[job_name] = PythonOperator(
-        task_id=job_name,
-        python_callable=get_glue_operator_args,
-        op_kwargs={'job_name':job_name},
-        dag=dag
-    )
+s3_client = boto3.resource('s3')
 
-# DAG structure is created by setting the upstream for all jobs
-for node in azk_flow['nodes']:
-    job_name = node['id']
-    if 'in' in node:
-        firing_job = node['in']
-        glue_tasks[job_name].set_upstream(glue_tasks[firing_job])
+try:
+    s3_client.Bucket(BUCKET_NAME).download_file(INPUT_WORKFLOW_FILE, INPUT_WORKFLOW_FILE)
+except botocore.exceptions.ClientError as e:
+    if e.response['Error']['Code'] == "404":
+        print("The object does not exist.")
+    else:
+        raise
+
+with open(INPUT_WORKFLOW_FILE) as f:
+    workflow_dict = json.load(f)
+    glue_tasks = dict()
+
+    for workflow_item in workflow_dict:
+        dag = DAG(
+            workflow_item["flow"],
+            default_args=default_args,
+            description='Glue job DAG generated from workflow json',
+            schedule_interval=timedelta(days=1),
+        )
+
+        # operator over the existing Glue jobs
+        for node in workflow_item['nodes']:
+            job_name = node['id']
+            glue_tasks[job_name] = PythonOperator(
+                task_id=job_name,
+                python_callable=get_glue_operator_args,
+                op_kwargs={'job_name':job_name},
+                dag=dag
+            )
+
+        # DAG structure is created by setting the upstream for all jobs
+        for node in workflow_item['nodes']:
+            job_name = node['id']
+            if 'in' in node:
+                firing_job_list = node['in']
+                for firing_job in firing_job_list:
+                    glue_tasks[job_name].set_upstream(glue_tasks[firing_job])
